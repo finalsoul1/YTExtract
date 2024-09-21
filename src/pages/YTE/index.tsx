3e2f7d33ff3e81895c9d606577aaf368c391fdb3
kwon <finalsoul1@gmail.com>
2024-09-21 19:09:45 +0900

    feat(yte): Flex 컴포넌트 적용

---
+++ src/pages/YTE/index.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '../../components/common/Flex'
+
+interface Props {}
+
+export const YTE = (_: Props) => {
+  return (
+    <Flex direction="column" align="center">
+      <h1>YTE</h1>
+      <input type="text" />
+    </Flex>
+  )
+}

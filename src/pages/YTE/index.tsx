935074bf30748cd356da0f6d2ee324e7e4cd0c86
kwon <finalsoul1@gmail.com>
2024-09-22 17:33:40 +0900

    feat(review): form 구조 만들기

--- src/pages/YTE/index.tsx
+++ src/pages/YTE/index.tsx
@@ -1,13 +1,61 @@
-import { Flex, Heading, Input, Stack } from '@chakra-ui/react'
+import { CheckIcon } from '@chakra-ui/icons'
+import {
+  Button,
+  Flex,
+  FormControl,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Stack,
+} from '@chakra-ui/react'
+import { isEmpty } from '@fxts/core'
+import { useForm } from 'react-hook-form'
+import { YTEFormValues } from './model'
 
-interface Props {}
+const URL_INPUT_ID = 'url-input-id'
 
-export const YTE = (_: Props) => {
+export const YTE = () => {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<YTEFormValues>()
+
+  console.log({ errors })
+
   return (
     <Flex justify="center" paddingTop={24}>
-      <Stack width={600} spacing={6}>
-        <Heading>YTE</Heading>
-        <Input placeholder="URL을 입력해주세요." size="lg" bg="white" />
+      <Stack width={600}>
+        <form
+          onSubmit={handleSubmit((data) => {
+            console.log({ data })
+          })}
+        >
+          <Stack spacing={6}>
+            <Heading>YTE</Heading>
+
+            <FormControl isInvalid={!isEmpty(errors)}>
+              <InputGroup size="lg">
+                <Input
+                  id={URL_INPUT_ID}
+                  placeholder="URL을 입력해주세요."
+                  bg="white"
+                  {...register('url', {
+                    required: 'url을 입력해주세요!',
+                  })}
+                />
+                <InputRightElement>
+                  <CheckIcon color="green.500" />
+                </InputRightElement>
+              </InputGroup>
+            </FormControl>
+
+            <Button type="submit" colorScheme="teal">
+              추출
+            </Button>
+          </Stack>
+        </form>
       </Stack>
     </Flex>
   )
---
+++ src/pages/YTE/model.ts
@@ -0,0 +1,3 @@
+export interface YTEFormValues {
+  url: string
+}
